b15fdcfca558dc45f26f9edb278cf1e802c14f4c
Hazle Bennard brune <hbennard@e2r8p3.21-school.ru>
2018-11-07 20:42:55 +0300

    ft_atoi.c

--- 1/ft_atoi.c
+++ 1/ft_atoi.c
@@ -1,40 +1,74 @@
 #include <unistd.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 int ft_atoi(char *str)
 {
 	int i;
 	int j;
 	int nb;
+	int sign;
 
 	i = 0;
-	j = 1;
-	nb = 1;
+	nb = 0;
+	sign = 1;
 
-	while (str[i] != '\0')
-	{
-		if (str[i] >= 0 && str[i] <= 32)
+	while (str[i] >= 0 && str[i] <= 32)
 		{
 			i++;
 		}
-		else if (str[i] == '-')
+	if (str[i] == '-')
 		{
-			nb = -nb;
+			sign = -1;
+			i++;
 		}
-		else if (str[i] >= 48 && str[i] <= 57)
+	while (str[i] >= 48 && str[i] <= 57)
 		{
-			nb = str[i] * j + 48;
+			nb = nb * 10 + str[i] - 48;
+			i++;
 		}
-		i++;
-		j*=10;
-	}
-	return (nb);
+	return (sign * nb);
 }
 
-int main (char *str)
+int main (void)
 {
-	str = "assdf 12312";
-	printf("%s\n", atoi(str));
-	printf("%s\n", ft_atoi(str));
+	char a[] = "   sdfsdft -5234AAAgreghrsth";
+	printf("%d\n", atoi(a));
+	printf("%d\n", ft_atoi(a));
 	return (0);
 }
+//
+//
+//#include <stdio.h> //
+//
+//int		ft_atoi(char *s)
+//{
+//	int		sign;
+//	long	r;
+//
+//	r = 0;
+//	sign = 1;
+//	while (*s == 32 || (*s >= 9 && *s <= 13))
+//		s++;
+//	if (*s == '-')
+//	{
+//		sign = -1;
+//		s++;
+//	}
+//	if (*s == '+')
+//		s++;
+//	while (*s >= '0' && *s <= '9')
+//	{
+//		r = r * 10 + *s - '0';
+//		s++;
+//	}
+//	return (sign * (int)r);
+//}
+//
+//int		main(void)//
+//{//
+//	char a[] = "    \n\n\v\f\r\t -5234AAAgreghrsth";// -5234
+//	printf("%d\n",ft_atoi(a));
+//	printf("%d\n", atoi(a));
+//}//
+
